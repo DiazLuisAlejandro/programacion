80efdf5a834b9ddb49d643fedc56d04a49d1a57d
DiazLuisAlejandro <alejandrodiazluisv@gmail.com>
2024-01-26 18:39:26 +0000

    Clase 26 Enero

--- Unidad-1/tarea/tareaLista/src/main/java/ies/puerto/AppOperacionesArrayList.java
+++ Unidad-1/tarea/tareaLista/src/main/java/ies/puerto/AppOperacionesArrayList.java
@@ -6,16 +6,17 @@ import java.util.ArrayList;
 
 public class AppOperacionesArrayList {
 
-    /**
+/**
     public static void main(String[] args) {
         ArrayList<String> list=new ArrayList<>();
         OperacionesArrayList textoejmplo =new OperacionesArrayList(list);
         int contador=0;
-        for (String cadena:textoejmplo.palabras) {
+        for (String cadena:textoejmplo.wordList) {
             if (cadena.matches("^[A|a].*")){
                 contador++;
             }
         }
         System.out.println("Hay "+contador+" que empiezen por A");
-    }**/
+    }
+ **/
 }
