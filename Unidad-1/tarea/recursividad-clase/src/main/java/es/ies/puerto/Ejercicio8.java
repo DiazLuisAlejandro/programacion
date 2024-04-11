d4badb70436d79b9e8825a0132f683c364e48fa1
DiazLuisAlejandro <113620789+DiazLuisAlejandro@users.noreply.github.com>
2024-04-11 23:37:56 +0100

    Tarea Recursividad 11 Abril

--- Unidad-1/tarea/recursividad-clase/src/main/java/es/ies/puerto/Ejercicio8.java
+++ Unidad-1/tarea/recursividad-clase/src/main/java/es/ies/puerto/Ejercicio8.java
@@ -1,8 +1,24 @@
 package es.ies.puerto;
 
 public class Ejercicio8 {
-
     public int sumaDigitos(int i){
-        return 0;
+        int resultado=0;
+        String num= String.valueOf(i);
+
+        String[] split = num.split("");
+
+
+        if (i==0){
+            return resultado;
+        }
+        i=((i-Integer.parseInt(split[split.length-1]))/10);
+
+        if (split.length>0){
+            resultado= Integer.parseInt(split[split.length-1]);
+            resultado+=sumaDigitos(i);
+
+        }
+
+        return resultado;
     }
 }
--- Unidad-1/tarea/recursividad-clase/src/main/java/es/ies/puerto/Ejercicio9.java
+++ Unidad-1/tarea/recursividad-clase/src/main/java/es/ies/puerto/Ejercicio9.java
@@ -1,4 +1,16 @@
 package es.ies.puerto;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Ejercicio9 {
+
+    public List<Integer> serieNumeros(int numero, int contador){
+        List<Integer> lista=new ArrayList<>();
+        lista.add(numero);
+        if (contador>0){
+            lista.addAll(serieNumeros((numero+(int)(10*Math.random())), contador-1));
+        }
+        return  lista;
+    }
 }
